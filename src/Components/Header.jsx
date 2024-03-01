6cadb7eee01e6b2615f9bfb4855a9e1ea159a451
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-03-01 11:34:50 +0530

    feat: header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header className='bg-darkPrimary px-10 py-4'>
+        <h1 className='font-black text-2xl text-lightPrimary'>AnonNest</h1>
+        
+    </header>
+    <hr className='border border-lightPrimary'/>
+    </>
+  )
+}
+
+export default Header
