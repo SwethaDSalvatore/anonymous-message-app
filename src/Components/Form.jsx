3596249141b4bf1d4607ba4dd41d3810a6642f73
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-03-01 14:57:29 +0530

    feat:adding and getting data

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,33 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { db } from "../firebase/index";
+import { setDoc, doc, addDoc,collection,getDocs } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Form = () => {
+  const navigate = useNavigate()
+  const { register, handleSubmit,reset } = useForm();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const querySnapshot = await getDocs(collection(db, "messages"));
+      setData(querySnapshot.docs);
+    }
+    getData();
+  }, []);
+ console.log(data)
+
+  const formSubmit = async (data) => {
+    console.log(data);
+    const docRef = await addDoc(collection(db, "messages"), data);
+    
+    reset();
+    navigate("/sendmessage")
+  };
+
+  
+
   return (
-    <div className="bg-darkPrimary h-screen p-10">
-
-    <form action="" className="bg-darkSecondary bg-opacity-10 shadow-2xl max-w-3xl mx-auto p-10 ">
-        <h1 className="text-lightSecondary font-black text-2xl mb-5 text-center">Write a Note</h1>
-    <textarea
-           id="message"
+    <div className="bg-darkPrimary  p-10">
+      <form
+        onSubmit={handleSubmit(formSubmit)}
+        className="bg-darkSecondary bg-opacity-10 shadow-2xl max-w-3xl mx-auto p-10 "
+      >
+        <h1 className="text-lightSecondary font-black text-2xl mb-5 text-center">
+          Write a Note
+        </h1>
+        <textarea
+          id="message"
+          name="message"
+          {...register("message")}
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 outline-none"
-            placeholder="Write your thoughts here..."
-          ></textarea>
-          <button className="px-6 py-2 hover:text-darkSecondary rounded-lg mt-4 shadow font-semibold bg-lightPrimary w-full">
-           Add Message
-          </button>
-    </form>
-    <section className="max-w-6xl m-10 mx-auto">
+          placeholder="Write your thoughts here..."
+        ></textarea>
+        <button className="px-6 py-2 hover:text-darkSecondary rounded-lg mt-4 shadow font-semibold bg-lightPrimary w-full">
+          Add Message
+        </button>
+      </form>
+
+      {data.map((data,index)=>{
+      
+        return (
+           <section className="max-w-6xl m-5 mx-auto">
            <blockquote class="p-4 my-4 border-s-4 border-gray-400 bg-gray-100 rounded">
-             <p class="italic font-medium leading-relaxed text-gray-900 ">
-              Flowbite is just awesome. It contains tons of predesigned
-              components and pages starting from login screen to complex
-              dashboard. Perfect choice for your next SaaS application.
+            <p class="italic font-medium leading-relaxed text-gray-900 ">
+          {data.data().message}
             </p>
-         </blockquote>
-        </section>
+          </blockquote>
+         </section>
+        )
+      })}
+     
     </div>
-
-
-
   );
 };
 
